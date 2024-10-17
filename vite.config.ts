f72a0104970217bb55eb55e8831b3d108ba73eb0
pro-3221 <150219280+pro-3221@users.noreply.github.com>
2024-10-17 17:56:25 +0330

    view

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // نام مخزن GitHub خود را اینجا وارد کنید
+  base: "/portfolio/", // اینجا باید آدرس repository را مشخص کنی
 });
